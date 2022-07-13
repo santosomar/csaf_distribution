817e13665a377f15f7b4f9bc3e5931087abcff70
Jan Höfelmeyer <Jan Hfelmeyer jhoefelmeyer@intevation.de>
2022-07-13 13:53:17 +0200

    Fixed direction of redirects in output

--- cmd/csaf_checker/reporters.go
+++ cmd/csaf_checker/reporters.go
@@ -85,7 +85,7 @@ func (r *redirectsReporter) report(p *processor, domain *Domain) {
 	}
 	sort.Strings(keys)
 	for i, k := range keys {
-		keys[i] = fmt.Sprintf("Redirect %s to %s", k, strings.ReplaceAll(p.redirects[k], ",", ", then to "))
+		keys[i] = fmt.Sprintf("Redirect %s to %s", strings.ReplaceAll(p.redirects[k], ",", ", to "), k)
 	}
 	req.message(WarnType, keys...)
 }
